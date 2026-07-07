81f72581df18ffd9f93f53892b25a5b5845e2864
silmo-yokohama <silmo.yokohama@gmail.com>
2026-07-07 15:48:48 +0900

    Wire recipe schema into Astro Content Collections

---
+++ viewer/src/content.config.ts
@@ -0,0 +1,10 @@
+import { defineCollection } from 'astro:content';
+import { glob } from 'astro/loaders';
+import { recipeSchema } from './schemas/recipe';
+
+const recipes = defineCollection({
+  loader: glob({ pattern: '**/*.json', base: '../../recipes' }),
+  schema: recipeSchema,
+});
+
+export const collections = { recipes };
